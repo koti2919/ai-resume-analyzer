5c49a83b919f78168283d7e13d764e6106d9c6e3
Annem Kotireddy <kotireddy5847@gmail.com>
2026-09-26 12:44:49 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -76,7 +76,7 @@ function App() {
       formData.append("job_description", jobDescription);
 
       const response = await fetch(
-        "http://127.0.0.1:8000/analyze-resume",
+        "https://ai-resume-analyzer-kggz.onrender.com/analyze-resume",
         {
           method: "POST",
           body: formData,
